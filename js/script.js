5e143439b641063f985ac85b6b2410d06752daac
DeversGreenGV <86885053+DeversGreenGV@users.noreply.github.com>
2022-09-30 14:23:10 +0300

    Sports1.0 (Pre-loader ON)

--- js/script.js
+++ js/script.js
@@ -1,48 +1,51 @@
 /* ////////////////////////////////////////// Pre - loaders */
+// document.body.classList.add('module');
+window.STORAGE = {}
+STORAGE.mask  = document.querySelector('#mask');
 
-// window.STORAGE = {}
-// STORAGE.mask  = document.querySelector('#mask');
-// STORAGE.stop = false;
-// STORAGE.maskTelefon = document.querySelector('.maskTelefon');
-// STORAGE.maskContPulse = document.querySelector('.maskContPulse');
-// function AnimationPulse() {
-//     if (STORAGE.stop) {
-//         setTimeout(() => {
-//             STORAGE.maskTelefon.style.transform = "scale(1)"
-//             STORAGE.maskContPulse.style.transform = "scale(1)"
-//         },100);
+STORAGE.stop = false;
+STORAGE.maskTelefon = document.querySelector('.maskTelefon');
+STORAGE.maskContPulse = document.querySelector('.maskContPulse');
+function AnimationPulse() {
+    if (!STORAGE.stop) {
+        setTimeout(() => {
+            STORAGE.maskTelefon.style.transform = "scale(0.6)"
+            STORAGE.maskContPulse.style.transform = "scale(0.6)"
+            STORAGE.mask.style.overflow = "hidden";
+        },100);
     
 
 
-//         STORAGE.maskPulse = STORAGE.maskContPulse.querySelector('.maskPulse');
-//         STORAGE.maskPulse.classList.remove('active');
-//         console.log(STORAGE.maskPulse," "+1);
-//         STORAGE.maskPulseCopy =STORAGE.maskPulse;
-//         console.log(STORAGE.maskPulseCopy," "+2 );
-//         setTimeout(() => {
-//             STORAGE.maskPulse.classList.add('active');
-//         },10);
+        STORAGE.maskPulse = STORAGE.maskContPulse.querySelector('.maskPulse');
+        STORAGE.maskPulse.classList.remove('active');
+        console.log(STORAGE.maskPulse," "+1);
+        STORAGE.maskPulseCopy =STORAGE.maskPulse;
+        console.log(STORAGE.maskPulseCopy," "+2 );
+        setTimeout(() => {
+            STORAGE.maskPulse.classList.add('active');
+        },10);
 
-//         setTimeout(() => {
-//             STORAGE.maskPulse.remove();
-//             STORAGE.maskPulseCopy.classList.remove('active');
-//             console.log(STORAGE.maskPulseCopy," "+3  );
+        setTimeout(() => {
+            STORAGE.maskPulse.remove();
+            STORAGE.maskPulseCopy.classList.remove('active');
+            console.log(STORAGE.maskPulseCopy," "+3  );
 
-//             let NewContPulse = STORAGE.maskContPulse.insertAdjacentElement("afterbegin", STORAGE.maskPulseCopy);
-//             setTimeout(() => {
-//                 STORAGE.maskTelefon.style.transform = "scale(1.1)"
-//                 STORAGE.maskContPulse.style.transform = "scale(1.1)"
-//                 AnimationPulse();  
-//             },30);
+            let NewContPulse = STORAGE.maskContPulse.insertAdjacentElement("afterbegin", STORAGE.maskPulseCopy);
+            setTimeout(() => {
+                STORAGE.maskTelefon.style.transform = "scale(0.7)"
+                STORAGE.maskContPulse.style.transform = "scale(0.7)"
+                AnimationPulse();  
+            },30);
         
-//         },2000);
-//     }
-// }
-// AnimationPulse();
+        },2000);
+    }
+}
+AnimationPulse();
 
 
-// window.addEventListener('load', () => {
-//     STORAGE.stop = true;
-//     STORAGE.mask.classList.add("hide");
-// })
+window.addEventListener('load', () => {
+    STORAGE.stop = true;
+    STORAGE.mask.classList.add("hide");
+    document.body.classList.remove('module');
+})
 /* ////////////////////////////////////////// Pre - loaders */
